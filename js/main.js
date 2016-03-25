b1a9bdae37110899aeaf9a88b831fcd66737b242
William McGann <contact@WilliamMcGann.com>
2016-03-25 17:41:14 -0400

    Optimizing a bit.

--- js/main.js
+++ js/main.js
@@ -2,50 +2,50 @@ $(window).load(function() {
 
 
 	//PRELOADER
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website.
 
 
-	$('#wrapper').bind('easytabs:after', function() {
-
-		// PORTFOLIO ISOTOPE
-		$(".portfolio_items").isotope({
-			itemSelector: '.single_item',
-			layoutMode: 'fitRows',
-			columnWidth: '.col-sm-3'
-		});
-
-		// isotope click function
-
-		$('.portfolio_filter ul li').click(function(){
-			$(".portfolio_filter ul li").removeClass("select-cat");
-			$(this).addClass("select-cat");
-
-			var selector = $(this).attr('data-filter');
-			$(".portfolio_items").isotope({
-				filter: selector,
-				animationOptions: {
-					duration: 750,
-					easing: 'linear',
-					queue: false,
-				}
-		});
-			return false;
-		});
-
-
-		// MAGNIFIC POPUP FOR PORTFOLIO PAGE
-		$('.image-link').magnificPopup({
-			type:'image',
-			gallery:{
-				enabled:true
-		    }
-		});
-
-
-
-
-
-	});
+	// $('#wrapper').bind('easytabs:after', function() {
+	//
+	// 	// PORTFOLIO ISOTOPE
+	// 	$(".portfolio_items").isotope({
+	// 		itemSelector: '.single_item',
+	// 		layoutMode: 'fitRows',
+	// 		columnWidth: '.col-sm-3'
+	// 	});
+	//
+	// 	// isotope click function
+	//
+	// 	$('.portfolio_filter ul li').click(function(){
+	// 		$(".portfolio_filter ul li").removeClass("select-cat");
+	// 		$(this).addClass("select-cat");
+	//
+	// 		var selector = $(this).attr('data-filter');
+	// 		$(".portfolio_items").isotope({
+	// 			filter: selector,
+	// 			animationOptions: {
+	// 				duration: 750,
+	// 				easing: 'linear',
+	// 				queue: false,
+	// 			}
+	// 	});
+	// 		return false;
+	// 	});
+	//
+	//
+	// 	// MAGNIFIC POPUP FOR PORTFOLIO PAGE
+	// 	$('.image-link').magnificPopup({
+	// 		type:'image',
+	// 		gallery:{
+	// 			enabled:true
+	// 	    }
+	// 	});
+	//
+	//
+	//
+	//
+	//
+	// });
 
 
 });
